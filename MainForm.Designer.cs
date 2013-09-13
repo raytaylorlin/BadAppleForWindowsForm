34b99d3bf371e0b7cc4f29718514e3080e615104
水月轻枫 <raytaylorlin@gmail.com>
2013-09-13 17:30:55 +0800

    初版提交

---
+++ MainForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： Administrator
+ * 日期: 2013/9/13
+ * 时间: 15:06
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+namespace BadAppleForWindowsForm
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+        {
+			this.pbxCanvas = new System.Windows.Forms.PictureBox();
+			this.lblIntroduce = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pbxCanvas)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pbxCanvas
+			// 
+			this.pbxCanvas.Location = new System.Drawing.Point(6, 6);
+			this.pbxCanvas.Margin = new System.Windows.Forms.Padding(2);
+			this.pbxCanvas.Name = "pbxCanvas";
+			this.pbxCanvas.Size = new System.Drawing.Size(640, 723);
+			this.pbxCanvas.TabIndex = 0;
+			this.pbxCanvas.TabStop = false;
+			// 
+			// lblIntroduce
+			// 
+			this.lblIntroduce.AutoSize = true;
+			this.lblIntroduce.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.lblIntroduce.Location = new System.Drawing.Point(674, 9);
+			this.lblIntroduce.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.lblIntroduce.MaximumSize = new System.Drawing.Size(345, 0);
+			this.lblIntroduce.Name = "lblIntroduce";
+			this.lblIntroduce.Size = new System.Drawing.Size(333, 224);
+			this.lblIntroduce.TabIndex = 1;
+			this.lblIntroduce.Text = "Bad Apple原曲旋律出自“東方”系列游戏第四作《東方幻想乡》，此曲因网上流传的PV黑白影绘动画而一炮走红，后续便有许多爱好者利用根据原视频创作出各种复刻版，" +
+			"如扫雷版、TXT版、WORD版等等。本DEMO是思存工作室成员出于业余爱好制作的Winform版。";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Black;
+			this.ClientSize = new System.Drawing.Size(1018, 740);
+			this.Controls.Add(this.lblIntroduce);
+			this.Controls.Add(this.pbxCanvas);
+			this.Font = new System.Drawing.Font("新宋体", 12F);
+			this.ForeColor = System.Drawing.Color.White;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+			this.Name = "MainForm";
+			this.Text = "BadApple";
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+			this.Load += new System.EventHandler(this.MainForm_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pbxCanvas)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+        }
+		
+		private System.Windows.Forms.PictureBox pbxCanvas;
+        private System.Windows.Forms.Label lblIntroduce;
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,134 @@
+﻿/*
+ * Bad Apple（Windows Form版）
+ * @author Ray Taylor Lin
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Media;
+using System.Windows.Forms;
+
+namespace BadAppleForWindowsForm
+{
+	public partial class MainForm : Form
+	{
+		private const int NUM_FRAME = 6517;
+		private const int LINES_PER_FRAME = 35;
+		private const string RESOURCE_FILE = "resource.dat";
+        
+        private static int playRate = 0;
+        private static int playDelay = 0;
+        
+
+        private int drawCount = 0;
+        private SoundPlayer sp = new SoundPlayer("bad apple.wav");
+        private SolidBrush drawBrush = new SolidBrush(Color.White);
+        private Graphics graphicsCanvas;
+        
+        private string strIntroduce = @"";
+        private string[] textOutput = new string[NUM_FRAME];
+		
+        /// <summary>
+        /// 构造方法
+        /// </summary>
+		public MainForm()
+		{
+			InitializeComponent();
+//			this.lblIntroduce.Text = this.strIntroduce;
+            this.ReadConfig();
+            this.LoadResource();
+		}
+		
+		/// <summary>
+		/// 窗体加载事件
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void MainForm_Load(object sender, EventArgs e)
+        {
+			//加载绘制的画布
+            this.graphicsCanvas = this.pbxCanvas.CreateGraphics();
+            //开始播放Bad Apple
+            this.PlayBadApple();
+        }
+		
+		private void PlayBadApple()
+        {
+            this.drawCount = 0;
+            Timer timer = new Timer();
+            timer.Interval = playRate;
+            timer.Tick += new EventHandler(timer_Tick);
+            this.sp.Stop();
+            this.sp.Play();
+            System.Threading.Thread.Sleep(playDelay);
+            timer.Start();
+        }
+		
+		void timer_Tick(object sender, EventArgs e)
+        {
+            if (this.drawCount < NUM_FRAME)
+            {
+                Bitmap drawBitmap = new Bitmap(1024, 768);
+                this.graphicsCanvas = Graphics.FromImage(drawBitmap);
+                this.graphicsCanvas.DrawString(textOutput[this.drawCount], this.Font, this.drawBrush, 0f, 0f);
+                this.pbxCanvas.Image = drawBitmap;
+                this.drawCount++;
+            }
+            else
+            {
+                Timer timerSender = (Timer)sender;
+                timerSender.Stop();
+                Timer timerDelay = new Timer() { Interval = 5000 };
+                timerDelay.Tick += new EventHandler((o, ea) => {
+                    this.PlayBadApple();
+                    timerDelay.Stop();
+                });
+                timerDelay.Start();
+                
+            }
+        }
+		
+		/// <summary>
+		/// 读取配置文件
+		/// </summary>
+		private void ReadConfig()
+        {
+            IniFileHelper iniFileHelper = new IniFileHelper(Environment.CurrentDirectory + @"\config.ini");
+            playRate = Convert.ToInt32(iniFileHelper.IniReadValue("main", "PlayRate"));
+            playDelay = Convert.ToInt32(iniFileHelper.IniReadValue("main", "PlayDelay"));
+        }
+		
+		/// <summary>
+		/// 读取资源文件
+		/// </summary>
+		private void LoadResource()
+        {
+			//初始化数组
+            for (int i = 0; i < NUM_FRAME; i++)
+            {
+                textOutput[i] = "";
+            }
+            
+            //读取字符串
+            StreamReader sr = new StreamReader(RESOURCE_FILE);
+            for (int i = 0; i < NUM_FRAME; i++)
+            {
+                for (int j = 0; j < LINES_PER_FRAME; j++)
+                {
+                    string tempStr=sr.ReadLine();
+                    textOutput[i] += tempStr;
+                    textOutput[i] += '\n';
+                }
+                sr.ReadLine();
+                sr.ReadLine();
+            }
+            sr.Close();
+            
+            //读取音乐文件
+            
+            
+            MessageBox.Show("Load completed!");
+        }
+	}
+}
